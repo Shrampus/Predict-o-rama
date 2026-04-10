5895878cb009980bf3e85654a4efb2bbf4b3be78
GhanBuriGhan <moist_daddy@LAPTOP-C5EA37F4.localdomain>
2026-04-10 09:13:39 +0000

    eemaldasin ebavajaliku impordi

--- backend/src/main/java/com/predictorama/backend/adapter/persistence/adapter/MatchRepositoryAdapter.java
+++ backend/src/main/java/com/predictorama/backend/adapter/persistence/adapter/MatchRepositoryAdapter.java
@@ -19,7 +19,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
