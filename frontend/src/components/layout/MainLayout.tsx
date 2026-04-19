e8092b393ee6bd67a411525f24739ddd30929cf2
eiramada <113309511+eiramada@users.noreply.github.com>
2026-04-19 17:48:19 +0300

    #67 remove tournaments link from MainLayout component

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -38,15 +38,6 @@ export function MainLayout() {
             </NavLink>
 
             <NavLink
-              to={ROUTE_PATHS.tournaments}
-              className={({ isActive }) =>
-                `nav-link ${isActive ? 'nav-link-active' : 'nav-link-default'}`
-              }
-            >
-              {t('nav.tournaments')}
-            </NavLink>
-
-            <NavLink
               to={ROUTE_PATHS.groups}
               className={({ isActive }) =>
                 `nav-link ${isActive ? 'nav-link-active' : 'nav-link-default'}`
